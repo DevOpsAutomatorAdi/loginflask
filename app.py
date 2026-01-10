1bd6112b827bd3188cb6cc27bedada2f5a0c7862
Aditya <156391626+DevOpsAutomatorAdi@users.noreply.github.com>
2026-01-10 20:26:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,30 +5,40 @@ import re
 import os
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
+# Load environment variables from .env file (works locally)
 load_dotenv()
 
 app = Flask(__name__)
 
-# Set configurations from environment variables
+# Secret key
 app.secret_key = os.getenv('SECRET_KEY')
-app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+
+# MySQL Configuration (IMPORTANT for Docker)
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')        # mysql-db (container name)
 app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+app.config['MYSQL_PORT'] = 3306                            # FORCE TCP (no socket)
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
+# Initialize MySQL
 mysql = MySQL(app)
 
-@app.route('/') 
+@app.route('/')
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     msg = ''
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
         username = request.form['username']
         password = request.form['password']
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password,))
+
+        cursor = mysql.connection.cursor()
+        cursor.execute(
+            'SELECT * FROM accounts WHERE username = %s AND password = %s',
+            (username, password,)
+        )
         account = cursor.fetchone()
+
         if account:
             session['loggedin'] = True
             session['id'] = account['id']
@@ -37,6 +47,7 @@ def login():
             return render_template('index.html', msg=msg)
         else:
             msg = 'Incorrect username/password!'
+
     return render_template('login.html', msg=msg)
 
 @app.route('/logout')
@@ -49,13 +60,19 @@ def logout():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     msg = ''
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+    if request.method == 'POST' and \
+       'username' in request.form and \
+       'password' in request.form and \
+       'email' in request.form:
+
         username = request.form['username']
         password = request.form['password']
         email = request.form['email']
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+
+        cursor = mysql.connection.cursor()
         cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
         account = cursor.fetchone()
+
         if account:
             msg = 'Account already exists!'
         elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
@@ -65,11 +82,16 @@ def register():
         elif not username or not password or not email:
             msg = 'Please fill out the form!'
         else:
-            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password, email,))
+            cursor.execute(
+                'INSERT INTO accounts VALUES (NULL, %s, %s, %s)',
+                (username, password, email,)
+            )
             mysql.connection.commit()
             msg = 'You have successfully registered!'
+
     elif request.method == 'POST':
         msg = 'Please fill out the form!'
+
     return render_template('register.html', msg=msg)
 
 if __name__ == '__main__':
