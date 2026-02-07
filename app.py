604784676ea06efa67c2922ee162825165c42e20
Aditya <156391626+DevOpsAutomatorAdi@users.noreply.github.com>
2026-02-07 15:39:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -108,4 +108,4 @@ def register():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Creates tables
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=8085)
